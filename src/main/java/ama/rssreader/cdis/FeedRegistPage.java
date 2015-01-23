e41a764aab74b66bb4dbdf0484dfecf60b90b16a
ama_nobu <nqf33359@nifty.com>
2015-01-23 15:41:34 +0900

    新規登録後、記事の確認するコードを追加

--- src/main/java/ama/rssreader/cdis/FeedRegistPage.java
+++ src/main/java/ama/rssreader/cdis/FeedRegistPage.java
@@ -1,11 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ama.rssreader.cdis;
 
 import ama.rssreader.ejbs.RssReaderLogic;
+import ama.rssreader.entities.Feedtbl;
+import ama.rssreader.util.LogUtil;
 import ama.rssreader.util.RssUtil;
 import ama.rssreader.validator.Feed;
 import java.io.Serializable;
@@ -14,8 +11,10 @@ import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.Pattern;
 
@@ -26,15 +25,17 @@ import javax.validation.constraints.Pattern;
 @Named(value = "feedRegistPage")
 @RequestScoped
 public class FeedRegistPage implements Serializable {
+
     //Validatorは上から順にではなく全てチェックされるみたいっすね
     //Groupってのを指定すると細かく制御出来る模様
     //つうじょうは暗黙的にjavax.validation.groups.Defaultが設定されているらしい
     @Pattern(regexp = "^http.*", message = "URLの形式がおかしいよ(httpで始まっていない)")
-    @Feed(message="おかしくないっすか？")
+    @Feed(message = "おかしくないっすか？")
     private String url;
     @EJB
     RssReaderLogic rsslogic;
-
+    @Inject
+    RssReaderLogicCDI rsslogiccdi;
     /**
      * Creates a new instance of FeedRegistPage
      */
@@ -43,7 +44,7 @@ public class FeedRegistPage implements Serializable {
 
     //@Pattern(regexp = "feedreg-success",message="登録失敗！")
     //こうやったら画面遷移せずに入力チェックエラーと同じ感じになると思ったけどならない
-    public String registFeed() {
+    public void registFeed() {
         try {
             String title = RssUtil.getTitle(getUrl());
 
@@ -53,11 +54,17 @@ public class FeedRegistPage implements Serializable {
             String userid = request.getRemoteUser();
             Logger.getLogger(FeedListPage.class.getName()).log(Level.SEVERE, "getFeedList called:" + request.getRemoteUser() + "," + title + "," + url, "");
             rsslogic.regFeed(userid, url);
-            //この様にxhtmlの名前にするのその画面がひょうじさ
-            return "feedreg-success";
+            Feedtbl feed = rsslogic.getFeed(userid, url);
+            //この様にxhtmlの名前にするのその画面がひょうじされる
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "登録されました", feed.getTitle()+"/"+url);
+            FacesContext.getCurrentInstance().addMessage(null, message);
+            rsslogiccdi.updateFeed(feed.getRssid());
+            LogUtil.log(FeedListPage.class.getName(), Level.INFO, feed.getRssid(),feed.getTitle(),feed.getUrl());
+            //return "FeedList?faces-redirect=true";
         } catch (Exception e) {
             e.printStackTrace();
-            return "FeedRegistError";
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "登録出来ませんでした", url);
+            FacesContext.getCurrentInstance().addMessage(null, message);
         }
     }
 
