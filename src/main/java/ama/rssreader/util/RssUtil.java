3083fa21c1a254d69a3c08c8519faf8a9da98e1c
ama_nobu <nqf33359@nifty.com>
2015-02-18 11:26:12 +0900

    jsoupを使ってFeedの内容で問題無さそうなtagだけ内容表示する用にする修正

--- src/main/java/ama/rssreader/util/RssUtil.java
+++ src/main/java/ama/rssreader/util/RssUtil.java
@@ -8,8 +8,24 @@ import com.rometools.rome.io.XmlReader;
 import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
+import javax.inject.Named;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.safety.Cleaner;
+import org.jsoup.safety.Whitelist;
 
+/**
+ * 各種Utilクラス
+ *
+ * @Named(value = "utilBean")としたのはJSFから呼べるようにした為
+ * @author amanobu
+ */
+@Named(value = "utilBean")
 public class RssUtil {
+
+    //ホワイトリスト
+    private static final Whitelist WHITELIST = buildWhiteList();
 
     public static void main(String args[]) throws Exception {
         String url = "http://rss.rssad.jp/rss/gihyo/feed/rss1";
@@ -36,7 +52,9 @@ public class RssUtil {
             System.out.println("publicsh:" + entry.getPublishedDate());
             // 記事の投稿日
             System.out.println("update date:" + entry.getUpdatedDate());
+
         }
+
     }
 
     public static String getTitle(String url) throws Exception {
@@ -67,12 +85,63 @@ public class RssUtil {
                 }
             }
             rssfeed.setContents(description);
-            
+
             //rssfeed.setUpddate(entry.getUpdatedDate());
             rssfeed.setPublishdate(entry.getPublishedDate());
             //LogUtil.log(RssUtil.class.getName(), Level.FINE, "feed::::::::", entry.getTitle(), entry.getLink(), entry.getDescription(), description,entry.getUpdatedDate(),entry.getPublishedDate());
             list.add(rssfeed);
         }
         return list;
+    }
+
+    /**
+     * ホワイトリストで定義されたタグは安全と見なし、それ以外は削除
+     *
+     * @param original_contents
+     * @return
+     */
+    public String nomalizeFeedContents(String original_contents) {
+        Document dirty = Jsoup.parse(original_contents);
+        Cleaner cleaner = new Cleaner(WHITELIST);
+        Document clean = cleaner.clean(dirty);
+        Element body = clean.body();
+        return body.html();
+    }
+
+    /**
+     * ホワイトリスト定義
+     *
+     * @return
+     */
+    private static Whitelist buildWhiteList() {
+        Whitelist whitelist = new Whitelist();
+        whitelist.addTags("a", "b", "blockquote", "br", "caption", "cite", "code", "col", "colgroup", "dd", "div", "dl", "dt", "em", "h1",
+                "h2", "h3", "h4", "h5", "h6", "i", "li", "ol", "p", "pre", "q", "small", "strike", "strong", "sub", "sup",
+                "table", "tbody", "td", "tfoot", "th", "thead", "tr", "u", "ul", "img");
+
+        whitelist.addAttributes("div", "dir");
+        whitelist.addAttributes("pre", "dir");
+        whitelist.addAttributes("code", "dir");
+        whitelist.addAttributes("table", "dir");
+        whitelist.addAttributes("p", "dir");
+        whitelist.addAttributes("a", "href", "title");
+        whitelist.addAttributes("blockquote", "cite");
+        whitelist.addAttributes("col", "span", "width");
+        whitelist.addAttributes("colgroup", "span", "width");
+        whitelist.addAttributes("img", "align", "alt", "height", "src", "title", "width", "style");
+        whitelist.addAttributes("ol", "start", "type");
+        whitelist.addAttributes("q", "cite");
+        whitelist.addAttributes("table", "border", "bordercolor", "summary", "width");
+        whitelist.addAttributes("td", "border", "bordercolor", "abbr", "axis", "colspan", "rowspan", "width");
+        whitelist.addAttributes("th", "border", "bordercolor", "abbr", "axis", "colspan", "rowspan", "scope", "width");
+        whitelist.addAttributes("ul", "type");
+
+        whitelist.addProtocols("a", "href", "ftp", "http", "https", "mailto");
+        whitelist.addProtocols("blockquote", "cite", "http", "https");
+        whitelist.addProtocols("img", "src", "http", "https");
+        whitelist.addProtocols("q", "cite", "http", "https");
+
+        whitelist.addEnforcedAttribute("a", "target", "_blank");
+        return whitelist;
     }
 }
